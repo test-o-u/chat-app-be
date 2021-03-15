bd9e2825a5a7b4558726c771621275fb7bd224b0
thien-lebk <bhnhock@gmail.com>
2021-03-15 14:07:32 +0700

    add cors

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
+var cors = require('cors');
 
 require('./db/connect') 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
